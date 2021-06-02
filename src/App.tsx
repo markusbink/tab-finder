3266cbc957155ca0b8eb05fd6cbddc1381b08cb6
markusbink <markusbink@users.noreply.github.com>
2021-06-02 16:10:44 +0200

    Add UI refresh fix for external monitor on mac

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,48 @@ const App: React.FC = () => {
     chrome.tabs.query({}, tabs => {
       setTabCount(tabs.length);
       setTabs(tabs);
-      console.log(tabs);
     });
 
+
+  /**
+   * Temporary workaround for secondary monitors on MacOS where redraws don't happen
+   * @See https://bugs.chromium.org/p/chromium/issues/detail?id=971701
+   * Solution from: https://stackoverflow.com/questions/56500742/why-is-my-google-chrome-extensions-popup-ui-laggy-on-external-monitors-but-not
+   */
+  if (
+    // From testing the following conditions seem to indicate that the popup was opened on a secondary monitor
+    window.screenLeft < 0 ||
+    window.screenTop < 0 ||
+    window.screenLeft > window.screen.width ||
+    window.screenTop > window.screen.height
+  ) {
+    chrome.runtime.getPlatformInfo(function (info) {
+      if (info.os === 'mac') {
+        const fontFaceSheet = new CSSStyleSheet()
+        fontFaceSheet.insertRule(`
+          @keyframes redraw {
+            0% {
+              opacity: 1;
+            }
+            100% {
+              opacity: .99;
+            }
+          }
+        `)
+        fontFaceSheet.insertRule(`
+          html {
+            animation: redraw 1s linear infinite;
+          }
+        `)
+        // @ts-ignore
+        document.adoptedStyleSheets = [
+          // @ts-ignore
+          ...document.adoptedStyleSheets,
+          fontFaceSheet,
+        ]
+      }
+    })
+  }
   }, []);
 
   return (
