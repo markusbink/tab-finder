ba79b8f2e292e772740bab16fd72d982f5f63b53
markusbink <markusbink@users.noreply.github.com>
2021-06-02 17:32:28 +0200

    Add font

---
+++ src/assets/icons/Close.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const Close: React.FC = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="96" fill="none" stroke="#000000" stroke-miterlimit="10" strokeWidth="16"></circle><line x1="160" y1="96" x2="96" y2="160" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line><line x1="160" y1="160" x2="96" y2="96" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line></svg>
+    )
+}
