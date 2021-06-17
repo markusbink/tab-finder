3da9716850019d998c14008ac424415c947109c1
markusbink <markusbink@users.noreply.github.com>
2021-06-17 23:03:22 +0200

    Add drag and drop to move tabs

--- src/components/TabItem.tsx
+++ src/components/TabItem.tsx
@@ -9,80 +9,102 @@ interface TabItemProps {
     tab: chrome.tabs.Tab;
 }
 
-export const TabItem: React.FC<TabItemProps> = ({ tab }) => {
-    const [isMuted, setIsMuted] = useState<boolean>(false);
-    const [isSelected, setIsSelected] = useState<boolean>(false);
-    const {
-        selectedTabs,
-        setSelectedTabs,
-        setIsGroupActionBarVisible,
-        isGroupActionBarVisible,
-    } = useTabContext();
+interface IItemProps {
+    key?: number;
+    tabIndex?: number;
+    "aria-roledescription"?: string;
+    onKeyDown?: (e: React.KeyboardEvent) => void;
+    onWheel?: (e: React.WheelEvent) => void;
+    style?: React.CSSProperties;
+    ref?: React.RefObject<any>;
+}
 
-    // Initially check whether a tab is muted or not
-    useEffect(() => {
-        chrome.tabs.get(tab.id!, async (currentTab) => {
-            const muted = currentTab.mutedInfo?.muted;
-            setIsMuted(() => !!muted);
-        });
-    }, [tab.id, tab.groupId]);
-
-    // Deselect all tab items once a group action has been triggered
-    useEffect(() => {
-        if (!isGroupActionBarVisible) {
-            setIsSelected(false);
-        }
-    }, [isGroupActionBarVisible]);
-
-    // Select and highlight the currently clicked tab
-    const selectTab = (tabId: number) => {
-        setIsSelected((prevIsSelected) => !prevIsSelected);
-
-        let updatedTabs: number[] = [];
-        if (selectedTabs.includes(tabId)) {
-            updatedTabs = selectedTabs.filter((id) => id !== tabId);
-        } else {
-            updatedTabs = [...selectedTabs, tabId];
-        }
-        setSelectedTabs(updatedTabs);
-        updatedTabs.length > 1
-            ? setIsGroupActionBarVisible(true)
-            : setIsGroupActionBarVisible(false);
-    };
-
-    const onTabClicked = (
-        e: React.MouseEvent<HTMLLIElement, MouseEvent>,
-        tabId: number
+export const TabItem: React.FC<TabItemProps> = React.forwardRef(
+    (
+        {
+            tab,
+            ...props
+        }: IItemProps & {
+            tab: chrome.tabs.Tab;
+        },
+        ref: React.Ref<HTMLLIElement>
     ) => {
-        if (e.metaKey) {
-            selectTab(tabId);
-            return;
-        }
-        chrome.tabs.get(tabId, (tab) => {
-            Tab.goToTab(tab.index);
-        });
-    };
+        const [isMuted, setIsMuted] = useState<boolean>(false);
+        const [isSelected, setIsSelected] = useState<boolean>(false);
+        const {
+            selectedTabs,
+            setSelectedTabs,
+            setIsGroupActionBarVisible,
+            isGroupActionBarVisible,
+        } = useTabContext();
 
-    return (
-        <li
-            onClick={(e) => onTabClicked(e, tab.id!)}
-            className={`tab-item ${isSelected ? "selected" : ""} ${
-                tab.groupId !== -1 ? "grouped" : ""
-            }`}>
-            {tab.favIconUrl ? (
-                <img src={tab.favIconUrl} alt="Favicon of individual tab" />
-            ) : (
-                <span className="img-placeholder"></span>
-            )}
-            <h4>{Helper.truncate(tab.title!, 35)}</h4>
-            <div className="actions">
-                <ToggleAudioBtn
-                    tab={tab}
-                    isMuted={isMuted}
-                    setIsMuted={setIsMuted}
-                />
-                <CloseTabBtn tab={tab} />
-            </div>
-        </li>
-    );
-};
+        // Initially check whether a tab is muted or not
+        useEffect(() => {
+            chrome.tabs.get(tab.id!, async (currentTab) => {
+                const muted = currentTab.mutedInfo?.muted;
+                setIsMuted(() => !!muted);
+            });
+        }, [tab.id, tab.groupId]);
+
+        // Deselect all tab items once a group action has been triggered
+        useEffect(() => {
+            if (!isGroupActionBarVisible) {
+                setIsSelected(false);
+            }
+        }, [isGroupActionBarVisible]);
+
+        // Select and highlight the currently clicked tab
+        const selectTab = (tabId: number) => {
+            setIsSelected((prevIsSelected) => !prevIsSelected);
+
+            let updatedTabs: number[] = [];
+            if (selectedTabs.includes(tabId)) {
+                updatedTabs = selectedTabs.filter((id) => id !== tabId);
+            } else {
+                updatedTabs = [...selectedTabs, tabId];
+            }
+            setSelectedTabs(updatedTabs);
+            updatedTabs.length > 1
+                ? setIsGroupActionBarVisible(true)
+                : setIsGroupActionBarVisible(false);
+        };
+
+        const onTabClicked = (
+            e: React.MouseEvent<HTMLLIElement, MouseEvent>,
+            tabId: number
+        ) => {
+            if (e.metaKey) {
+                selectTab(tabId);
+                return;
+            }
+            chrome.tabs.get(tabId, (tab) => {
+                Tab.goToTab(tab.index);
+            });
+        };
+
+        return (
+            <li
+                ref={ref}
+                {...props}
+                onClick={(e) => onTabClicked(e, tab.id!)}
+                className={`tab-item ${isSelected ? "selected" : ""} ${
+                    tab.groupId !== -1 ? "grouped" : ""
+                }`}>
+                {tab.favIconUrl ? (
+                    <img src={tab.favIconUrl} alt="Favicon of individual tab" />
+                ) : (
+                    <span className="img-placeholder"></span>
+                )}
+                <h4>{Helper.truncate(tab.title!, 35)}</h4>
+                <div className="actions">
+                    <ToggleAudioBtn
+                        tab={tab}
+                        isMuted={isMuted}
+                        setIsMuted={setIsMuted}
+                    />
+                    <CloseTabBtn tab={tab} />
+                </div>
+            </li>
+        );
+    }
+);
--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -1,22 +1,47 @@
 import * as React from "react";
-import { useTabContext } from "../contexts/TabContext";
 import { TabItem } from "./TabItem";
+import { useTabContext } from "../contexts/TabContext";
+import { List, arrayMove } from "react-movable";
 
 interface TabListProps {
     tabs: chrome.tabs.Tab[];
 }
 
 export const TabList: React.FC<TabListProps> = ({ tabs }) => {
+    const { setTabs } = useTabContext();
+
+    const onTabItemMoved = (oldIndex: number, newIndex: number) => {
+        setTabs(arrayMove(tabs, oldIndex, newIndex));
+        chrome.tabs.query({ index: oldIndex }, (tabs) => {
+            if (!tabs) {
+                return;
+            }
+            const tabId: number = tabs[0].id!;
+            chrome.tabs.move(tabId, { index: newIndex });
+        });
+    };
+
     return (
         // Make tab groups lay on top of one another like a deck of cards
         // Make a range selection for groups
         // Make groups unselectable
         // Add differenct colours for different groups
         // Persist state
-        <ul className="tab-list">
-            {tabs.map((tab: chrome.tabs.Tab) => (
-                <TabItem key={tab.id} tab={tab} />
-            ))}
-        </ul>
+        <List
+            values={tabs}
+            onChange={({ oldIndex, newIndex }) =>
+                onTabItemMoved(oldIndex, newIndex)
+            }
+            renderList={({ children, props }) => (
+                <ul className="tab-list" {...props}>
+                    {children}
+                </ul>
+            )}
+            renderItem={({ value, props }) => (
+                <div {...props}>
+                    <TabItem tab={value} />
+                </div>
+            )}
+        />
     );
 };
