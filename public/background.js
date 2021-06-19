04cfccf1ca6063862857eeeb2b1b88aa45a7ad34
markusbink <markusbink@users.noreply.github.com>
2021-06-19 12:26:01 +0200

    Remove code from background and content script

--- public/background.js
+++ public/background.js
@@ -1,10 +1,1 @@
 console.log("background-script loaded...");
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.value) {
-    chrome.tabs.query({}, (tabs) => {
-      sendResponse({ tabs });
-    });
-  }
-  return true;
-});
